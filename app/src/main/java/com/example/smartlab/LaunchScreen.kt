05b05122ed1271d9a0b47413e0d0f9c94a8333c5
Stepan <stgalitsyn@gmail.com>
2023-03-04 23:11:21 +0300

    v1

---
+++ app/src/main/java/com/example/smartlab/LaunchScreen.kt
@@ -0,0 +1,11 @@
+package com.example.smartlab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LaunchScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch_screen)
+    }
+}
---
+++ app/src/main/java/com/example/smartlab/PatientRegScreen.kt
@@ -0,0 +1,11 @@
+package com.example.smartlab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PatientRegScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_reg_screen)
+    }
+}
