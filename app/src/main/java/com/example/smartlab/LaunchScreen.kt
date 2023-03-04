ea2b9295e3c25bee7c126da9df5295d9c71406c4
Stepan <stgalitsyn@gmail.com>
2023-03-04 23:37:13 +0300

    v1.1

--- app/src/main/java/com/example/smartlab/LaunchScreen.kt
+++ app/src/main/java/com/example/smartlab/LaunchScreen.kt
@@ -1,11 +1,25 @@
 package com.example.smartlab
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 
 class LaunchScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch_screen)
+
+        object: CountDownTimer(1500, 1500){
+            override fun onTick(p0: Long) {
+
+            }
+
+            override fun onFinish() {
+                val intent = Intent(this@LaunchScreen, PatientRegScreen::class.java)
+                startActivity(intent)
+                this@LaunchScreen.finish()
+            }
+        }.start()
     }
 }
